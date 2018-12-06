70814aae7beba0c0192db9c52d3678d636b5a3a8
Rich Wang <jen.wei.wang@duke.edu>
2018-12-06 01:12:45 -0500

    working software

--- Software/arduino/arduino.ino
+++ Software/arduino/arduino.ino
@@ -1,18 +1,17 @@
-//arduino
-
-#define PUBLISH_DELAY 150000    //2.5 min b/w variable publish
-
-
+//#define PUBLISH_DELAY 150000    //2.5 min b/w variable publish
 #define FLASH_RATE_HZ 2
-#define BUTTON_IN D1
-#define PWN_LED_OUT D11
+//#define BUTTON_IN 2
+//#define PWM_LED_OUT 10
 // use preprocessor directives to define constants in the code
-#define PWM_MAX 255
+//#define PWM_MAX 255
 
 // global variables representing "state" across all functions
 //operating mode: 0 - off, 1 - bright, 2 - mid-bright, 3- dim, 4- flash
+const int BUTTON_IN = 2;
 int BUTTON_PUSHED = 0;
-bool button_pushed = TRUE;
+const int PWM_LED_OUT = 10;
+int PWM_MAX = 255;
+//int button_pushed = 1;
 int operating_mode = 0;
 int previous_button_state = 0;
 int PWM_OUT = 0;
@@ -20,28 +19,24 @@ int ledState = LOW;
 unsigned long previousMillis = 0;
 
 
-
-
-
-
 void setup() {
-  pinMode(D2, INPUT);
-  pinMode(D10, OUTPUT);
-  attachInterrupt(BUTTON_IN, button_pushed, FALLING);
+  Serial.begin(9600);
+
+  pinMode(PWM_LED_OUT, OUTPUT);
+  pinMode(BUTTON_IN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_IN), button_pushed, FALLING);
 //define output(PWM) pin connected to LED
 }
 
 void loop() {
 
-  unsigned long currentMillis = millis();
+  //Serial.begin(9600);
 
-  Serial.begin(9600);
-
-  check_for_button_press();
+  check_button_press();
 
   set_pwm_based_on_operating_mode();
 
-  shine_led();
+  //shine_led();
 
 }
 
@@ -51,15 +46,19 @@ void set_pwm_based_on_operating_mode() {
     switch (operating_mode) {
         case 0:
             PWM_OUT = 0;
+            digitalWrite(PWM_LED_OUT, LOW);
             break;
         case 1:
             PWM_OUT = PWM_MAX;
+            shine_led();
             break;
         case 2:
-            PWN_OUT = int(PWM_MAX/2);
+            PWM_OUT = int(PWM_MAX/2);
+            shine_led();
             break;
         case 3:
-            PWN_OUT = int(PWM_MAX/4);
+            PWM_OUT = int(PWM_MAX/4);
+            shine_led();
             break;
         case 4:
             flash_the_light();
@@ -70,16 +69,13 @@ void set_pwm_based_on_operating_mode() {
 
 
 void button_pushed(){
-//BUTTON_PUSHED = 1;
-  BUTTON_PUSHED = TRUE;
-//void button_pushed() {
-//    BUTTON_PUSHED = true;
+  BUTTON_PUSHED = 1;
 }
 
 void flash_the_light(){
-  // check to see if it's time to blink the LED; that is, if the difference
-  // between the current time and last time you blinked the LED is bigger than
-  // the interval at which you want to blink the LED.
+  int interval = 1000/(FLASH_RATE_HZ);
+  unsigned long currentMillis = millis();
+
   if (currentMillis - previousMillis >= interval) {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
@@ -92,30 +88,28 @@ void flash_the_light(){
     }
 
     // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
+    digitalWrite(PWM_LED_OUT, ledState);
   }
 
 }
-
 
 void shine_led() {
   Serial.print(operating_mode);
 
   delay(100);
-  analogWrite(PWM_LED_OUT, outputPWM);
+  analogWrite(PWM_LED_OUT, PWM_OUT);
 
 }
-//void shine_led() {
-//    analogWrite(PWM_LED_OUT, PWM_OUT);
-//}
 
 void check_button_press() {
-    if (BUTTON_PUSHED == true) {
-        operating_mode = operating_mode + 1;
-        if (operating_mode == 5) {
+    if (BUTTON_PUSHED == 1) {
+        //operating_mode = operating_mode + 1;
+        if (operating_mode == 4) {
             operating_mode = 0;
         }
+        else {
+          operating_mode = operating_mode + 1;
+        }
     }
-
-    BUTTON_PUSHED = false
+    BUTTON_PUSHED = 0;
 }
